df4b54bd263b30ca7d24aa08599ca8b78fe53d79
EricDosReis <ericdosreis.personal@gmail.com>
2025-05-07 17:09:23 -0300

    feat: setup Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-
 function App() {
   return (
-    <h1>Tic Tac Toe</h1>
-  )
+    <h1 className="text-pink bg-blue-100 text-3xl font-bold underline">
+      Tic Tac Toe
+    </h1>
+  );
 }
 
-export default App
+export default App;
